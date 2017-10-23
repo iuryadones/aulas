4b2646a985040de2a7561fe742f6bf09d66d365e
Iury Adones Xavier dos Santos <iuryadones@gmail.com>
2017-10-22 22:10:22 -0300

    [master] aula-5 finalizada

--- web-django/examples/aula-5/xstock/stox/core/views.py
+++ web-django/examples/aula-5/xstock/stox/core/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Item
+
 
 def index(request):
-    return HttpResponse("Olá, mundo!")
+    itens = Item.objects.all()
+    template = 'core/index.html'
+    context = {'itens': itens}
+
+    return render(request, template, context)
 
--- web-django/examples/aula-5/xstock/stox/stox/urls.py
+++ web-django/examples/aula-5/xstock/stox/stox/urls.py
@@ -18,6 +18,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^', include('core.urls')),
     url(r'^core/', include('core.urls')),
     url(r'^admin/', admin.site.urls),
 ]
