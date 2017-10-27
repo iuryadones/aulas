9d2f272d33f9a04f78d20edd0220065d2c11f5cc
Iury Adones Xavier dos Santos <iuryadones@gmail.com>
2017-10-27 14:25:21 -0300

    [master] aula 6

---
+++ web-django/examples/aula-6/xstock/stox/core/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^item/(?P<pk>[0-9]+)/$', views.item_detail, name='item_detail'),
+    url(r'^contact/$', views.contact, name='contact'),
+]
