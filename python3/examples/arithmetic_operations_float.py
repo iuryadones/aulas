56bf06bde27782423c979b34a6815a244231fadb
Iury Adones Xavier dos Santos <iuryadones@gmail.com>
2018-03-17 14:45:34 -0300

    [master] Python arthmetic operations

---
+++ python3/examples/arithmetic_operations_float.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+real_1 = 2.5
+real_2 = 0.55
+
+result_1 = real_1 * real_2
+print(result_1)
+
+result_2 = real_1 ** real_2
+print(result_2)
+
+result_3 = real_1 + real_2
+print(result_3)
+
+result_4 = real_1 - real_2
+print(result_4)
+
+result_5 = real_1 / real_2
+print(result_5)
+
+result_6 = real_1 // real_2
+print(result_6)
+
+result_7 = real_1 % real_2
+print(result_7)
+
+result_8 = real_2 % real_1
+print(result_8)
---
+++ python3/examples/arithmetic_operations_string.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+string_1 = "Olá, "
+string_2 = "Mundo!"
+
+number_1 = 3
+number_2 = 2
+
+result_1 = string_1 + string_2
+print(result_1)
+
+result_2 = string_1 * number_1
+print(result_2)
+
+result_3 = string_1 * number_1 + string_2
+print(result_3)
+
+result_4 = str(number_1) + str(number_2) 
+print(result_4)
