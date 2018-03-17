e6ae199e15afe958af34772468b17ee932e2d7f7
Iury Adones Xavier dos Santos <iuryadones@gmail.com>
2018-03-17 15:18:24 -0300

    [master] Type string

--- python3/examples/type_string_3.py
+++ python3/examples/type_string_3.py
@@ -14,11 +14,14 @@ print(result_2)
 result_3 = "number: {}, real: {}".format(string_3, string_4)
 print(result_3)
 
-result_4 = "number: {num}, real: {real}".format(real=string_4, num=string_3)
+result_4 = "number: {1}, real: {0}".format(string_4, string_3)
 print(result_4)
 
-result_5 = f'number: {string_3}, real: {string_4}'
+result_5 = "number: {num}, real: {real}".format(real=string_4, num=string_3)
 print(result_5)
 
-result_6 = f"number: {int(string_3)}, real: {float(string_4)}"
+result_6 = f'number: {string_3}, real: {string_4}'
 print(result_6)
+
+result_7 = f"number: {int(string_3)}, real: {float(string_4)}"
+print(result_7)
