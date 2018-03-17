12566ca38cdb628a2305cec787ba09e897c00b95
Iury Adones Xavier dos Santos <iuryadones@gmail.com>
2018-03-17 15:10:00 -0300

    [master] Python conditionals

---
+++ python3/examples/conditionals.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+name_1 = "Marcus"
+name_2 = "Joe"
+string_1 = "olá, Marcus"
+string_2 = "olá, Joe"
+
+if "Marcus" == name_2:
+    print(string_1)
+else:
+    print("Desculpe, não entendo!")
+
+if "Marcus" == name_2:
+    print(string_1)
+elif "Joe" == name_2:
+    print(string_2)
+else:
+    print("Desculpe, não entendo!")
