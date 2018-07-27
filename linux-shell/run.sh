d0d84c681de2e416f0b109f4ffede2a16bb4ee20
Iury Adones Xavier dos Santos <iuryadones@gmail.com>
2018-07-27 16:12:47 -0300

    [master] Organiza todas as aulas

--- linux-shell/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# pandoc -t revealjs -s $1 -o $2 --highlight-style=pygments --self-contained --slide-level=2 --standalone --smart
-# pandoc -t revealjs -s $1 -o $2 --highlight-style=tango --self-contained --slide-level=2 --standalone --smart
-# pandoc -t revealjs -s $1 -o $2 --highlight-style=espresso --self-contained --slide-level=2 --standalone --smart
-pandoc -t revealjs -s $1 -o $2 --highlight-style=zenburn --self-contained --slide-level=2 --standalone --smart
-# pandoc -t revealjs -s $1 -o $2 --highlight-style=kate --self-contained --slide-level=2 --standalone --smart
-# pandoc -t revealjs -s $1 -o $2 --highlight-style=monochrome --self-contained --slide-level=2 --standalone --smart
-# pandoc -t revealjs -s $1 -o $2 --highlight-style=breezedark --self-contained --slide-level=2 --standalone --smart
-# pandoc -t revealjs -s $1 -o $2 --highlight-style=haddock --self-contained --slide-level=2 --standalone --smart
