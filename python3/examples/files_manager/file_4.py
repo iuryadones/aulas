aed60b4291bb3208fb67f5519078edab0f2dfbd0
Iury Adones Xavier dos Santos <iuryadones@gmail.com>
2018-08-09 20:36:44 -0300

    [master] Add exemplo de file com dict

--- python3/examples/files_manager/file_4.py
+++ python3/examples/files_manager/file_4.py
@@ -12,28 +12,61 @@ chamadado conte_vogais.dat:
 5 - mostrar a pessoa mais nova e a mais sabia da turma
 """
 
-arquivo = open("arquivo.dat")
-leitura = arquivo.read()
-arquivo.close()
+# arquivo = open("arquivo.dat")
+# leitura = arquivo.read()
+# arquivo.close()
 
 
-print(leitura, end="\n\n")
+# print(leitura, end="\n\n")
 
-vogais = ["a", "e", "i", "o", "u"]
+# vogais = ["a", "e", "i", "o", "u"]
 
-conte_vogais = open("conte_vogais.dat", "w")
-conte_vogais.close()
+# conte_vogais = open("conte_vogais.dat", "w")
+# conte_vogais.close()
 
-for letra in vogais:
-    print(f"vogal: {letra}\tquantidade: {leitura.count(letra)}")
+# for letra in vogais:
+#     print(f"vogal: {letra}\tquantidade: {leitura.count(letra)}")
 
-    conte_vogais = open("conte_vogais.dat", "a")
-    conte_vogais.write(f"{letra}\t{leitura.count(letra)}\n")
-    conte_vogais.close()
+#     conte_vogais = open("conte_vogais.dat", "a")
+#     conte_vogais.write(f"{letra}\t{leitura.count(letra)}\n")
+#     conte_vogais.close()
 
-ler = open("conte_vogais.dat")
-l = ler.read()
-ler.close()
+# ler = open("conte_vogais.dat")
+# l = ler.read()
+# ler.close()
 
-print(f"\n{l}\n")
+# print(f"\n{l}\n")
 
+base_nomes = {}
+conv = {0: "id", 1: "nome", 2: "idade"}
+
+arq = open("arquivo.dat")
+for line in arq.readlines():
+    for n, col in enumerate(line.split("\t")):
+        if conv.get(n) == "id":
+            _id = int(col)
+            base_nomes.update({_id: {}})
+        else:
+            base_nomes.get(_id).update({conv.get(n): col})
+
+# print(base_nomes)
+idades = []
+
+for key, value in base_nomes.items():
+    idades.append(int(value.get("idade").strip()))
+
+print(sum(idades))
+print(sum(idades) / len(idades))
+print(min(idades))
+print(max(idades))
+
+for key, value in base_nomes.items():
+
+    if int(value.get("idade").strip()) in (min(idades), max(idades)):
+        print(value.get("nome"))
+
+from pprint import pprint
+
+print(base_nomes, end="\n\n")
+
+pprint(base_nomes)
